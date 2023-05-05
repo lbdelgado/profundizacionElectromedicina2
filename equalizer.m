74bd0474e5f105a91d87182505dff6ab12966d7d
Breinar Delgado <lbdelgado5@misena.edu.co>
2023-05-05 10:29:35 -0500

    Punto del reto 1 sobre ECG

--- equalizer.m
+++ equalizer.m
@@ -1,21 +1,49 @@
 function [I] = equalizer(I)
-%Esta funcion ecualiza una imagen
-[m,n]=size(I);
+% %Esta funcion ecualiza una imagen
+% [m,n]=size(I);
+% 
+% %Adquiere el histograma de la imagen
+% h=imhist(I);
+% 
+% %Calcula la probabilidad de cada nivel
+% p=255*h/(m*n);
+% 
+% %Calcula el vector de sumas acumuladas, es decir la transformacion como tal
+% T=cumsum(p);
+% 
+% for i=1:m
+%     for j=1:n
+%         pixel=I(i,j);
+%         I(i,j)=T(pixel+1);
+%     end
+% end
 
-%Adquiere el histograma de la imagen
-h=imhist(I);
 
-%Calcula la probabilidad de cada nivel
-p=255*h/(m*n);
+H=imhist(I);
+H=H';
+suma=H(1,1);
+[N,M]=size(I);
+G=zeros(N,M);
 
-%Calcula el vector de sumas acumuladas, es decir la transformacion como tal
-T=cumsum(p);
+for k=1:255  % rango del histograma
+    H2(1,k)=suma;
+    suma=suma+H(1,k+1);    
+end
+plot(H2)
 
-for i=1:m
-    for j=1:n
-        pixel=I(i,j);
-        I(i,j)=T(pixel+1);
+np=N*M; % Número total de pixeles
+poc=double(H2./np); % valor entre 0 y 1
+cdf=double(round(poc.*255)); % 255 es lk
+plot(cdf)
+
+for i=1:N
+    for j=1:M
+        b=I(i,j);
+        if b==0
+            b=1;
+        end
+        G(i,j)=cdf(1,b);
     end
 end
+I=uint8(G);
 end
-
